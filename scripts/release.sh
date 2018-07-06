cfdc341cfe10940343d317ac15b51106861747b8
oss6 <ossedb@gmail.com>
2018-07-06 10:14:21 +0100

    Invert order

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Running release tasks..."
 
-python manage.py migrate
-python scripts/stanford_parser.py
 python -m spacy download en
+python scripts/stanford_parser.py
+python manage.py migrate
