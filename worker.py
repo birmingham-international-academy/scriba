4f1f2d55e3f6f1950e10d23094b4d9256a7ec540
oss6 <ossedb@gmail.com>
2018-07-06 16:34:35 +0100

    Fix

--- worker.py
+++ worker.py
@@ -2,8 +2,10 @@ import os
 import sys
 
 import django
-import redis
-from rq import Worker, Queue, Connection
+import urllib
+from redis import Redis
+from rq import Queue, Connection
+from rq.worker import HerokuWorker as Worker
 
 settings = 'scriba.settings.production'
 
@@ -16,9 +18,13 @@ django.setup()
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL')
+if not redis_url:
+    raise RuntimeError('Set up Redis To Go first.')
 
-conn = redis.from_url(redis_url)
+urllib.parse.uses_netloc.append('redis')
+url = urllib.parse.urlparse(redis_url)
+conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
     with Connection(conn):
