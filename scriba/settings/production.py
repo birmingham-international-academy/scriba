debb15f3a34451786be7dae3fa80b2024b8fd0b2
oss6 <ossedb@gmail.com>
2018-07-17 16:36:01 +0100

    Fix production settings

--- scriba/settings/production.py
+++ scriba/settings/production.py
@@ -1,5 +1,3 @@
-import dj_database_url
-
 from scriba.settings.base import *
 
 
@@ -10,16 +8,9 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '178.128.41.130',
     'englishapp.tk',
-    'www.englishapp.tk',
-    '.herokuapp.com'
+    'www.englishapp.tk'
 ]
 
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-
-"""
-DATABASES['default'] = dj_database_url.config(
-    default=os.environ.get('DATABASE_URL', '')
-)
-"""
