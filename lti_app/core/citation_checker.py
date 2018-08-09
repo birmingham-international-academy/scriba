770225bb3c86446e82ec216900d1735f993d8400
oss6 <ossedb@gmail.com>
2018-08-09 11:18:08 +0100

    Fix #16

--- lti_app/core/citation_checker.py
+++ lti_app/core/citation_checker.py
@@ -17,7 +17,38 @@ class Checker:
         self.text = text
         self.reference = reference
 
-    def _get_citation_patterns(self, authors, year):
+    def _get_citation_regexps(self, authors, year):
+        authors_regex = r'(?:' + re.escape(authors)
+
+        if ' & ' in authors:
+            authors_regex += (
+                r'|' + re.escape(authors.replace(' & ', ' and '))
+            )
+        elif ' and ' in authors:
+            authors_regex += (
+                r'|' + re.escape(authors.replace(' and ', ' & '))
+            )
+
+        authors_regex += r')'
+
+        in_text_regex = authors_regex + r'\s*\(\s*' + year + r'\s*\)'
+
+        in_text_separated_regex = (
+            r'((?!\()' + authors_regex + r'.*?(?!\(\s*)' + year + r'(?!\s*\)))'
+            + r'|(' + year + r'.*?' + authors_regex + r')'
+        )
+
+        parenthetical_regex = (
+            r'\(' + authors_regex + r'(?:\s*,\s*|\s)' + year + r'\)'
+        )
+
+        return {
+            'in_text': in_text_regex,
+            'in_text_separated': in_text_separated_regex,
+            'parenthetical': parenthetical_regex
+        }
+
+    def _get_example_citations(self, authors, year):
         return [
             authors + ' (' + year + ')',
             '(' + authors + ', ' + year + ')',
@@ -32,6 +63,8 @@ class Checker:
             and the extracted authors
         """
 
+        # Extract reference components
+        # ---------------------------------------------
         m = re.match(
             r'^(?P<authors>(?:(?:\w+,\s?(?:\w\.)+)(?:,\s?|\sand\s|,\s?and\s|\s&\s|,\s?&\s)?)+)\s?'
             r'\((?P<year>\d{4})\)\s'
@@ -52,6 +85,8 @@ class Checker:
         ]
         authors = authors[::2]
 
+        # Build last name part
+        # ---------------------------------------------
         lastnames_citation = ''
 
         if len(authors) >= 4:
@@ -65,39 +100,49 @@ class Checker:
                 elif index != len(authors) - 1:
                     lastnames_citation += ', '
 
-        possible_citations = self._get_citation_patterns(
+        # Get citation regular expressions and
+        # match them against the text
+        # ---------------------------------------------
+        regexps = self._get_citation_regexps(
             lastnames_citation,
             year
         )
 
-        if ' and ' in lastnames_citation:
-            lastnames_citation_ampersand = lastnames_citation.replace(
-                ' and ',
-                ' & '
-            )
-
-            possible_citations += self._get_citation_patterns(
-                lastnames_citation_ampersand,
-                year
-            )
-
-        result = False
-
+        # Match in text and parenthetical patterns
+        in_text_regex = regexps.get('in_text')
+        in_text_separated_regex = regexps.get('in_text_separated')
+        parenthetical_regex = regexps.get('parenthetical')
         text = self.text.lower()
-        possible_citations_lower = [cit.lower() for cit in possible_citations]
 
-        for cit in possible_citations_lower:
-            if cit in text:
-                result = True
-                break
+        in_text_matches = re.findall(in_text_regex, text, re.IGNORECASE)
+        in_text_separated_matches = re.findall(in_text_separated_regex, text, re.IGNORECASE)
+        parenthetical_matches = re.findall(parenthetical_regex, text, re.IGNORECASE)
 
-        # Check if citation is after a full stop.
-        last_sentence = text.split('.')[-1].strip()
-        citation_after_full_stop = last_sentence in possible_citations_lower
+        has_in_text_cit = len(in_text_matches) > 0
+        has_in_text_separated_cit = len(in_text_separated_matches) > 0
+        has_parenthetical_cit = len(parenthetical_matches) > 0
+        result = (
+            has_in_text_cit
+            or has_in_text_separated_cit
+            or has_parenthetical_cit
+        )
+
+        # Check if citation is after a full stop
+        citation_after_full_stop = re.findall(
+            r'\.\s?' + parenthetical_regex + r'$',
+            text,
+            re.IGNORECASE
+        )
+
+        # Get examples of citations (for feedback)
+        example_citations = self._get_example_citations(lastnames_citation, year)
 
         return {
             'result': result,
-            'possible_citations': possible_citations,
+            'in_text': has_in_text_cit,
+            'in_text_separated': has_in_text_separated_cit,
+            'parenthetical': has_parenthetical_cit,
+            'example_citations': example_citations,
             'citation_after_full_stop': citation_after_full_stop,
             'authors': authors,
             'year': year
--- lti_app/tests/test_citation_checker.py
+++ lti_app/tests/test_citation_checker.py
@@ -17,38 +17,123 @@ citation_check = [
     (
         'Edbali (2018) stated something.',
         'Edbali, T. (2018) "The test" Journal of Testing, 15(4): 261-278.',
-        {'authors': ['Edbali'], 'year': '2018', 'result': True}
+        {
+            'authors': ['Edbali'],
+            'year': '2018',
+            'result': True,
+            'in_text': True,
+            'in_text_separated': False,
+            'parenthetical': False
+        }
     ),
     (
         'The cat is brown (Edbali 2018).',
         'Edbali, T. (2018) "The test" Journal of Testing, 15(4): 261-278.',
-        {'authors': ['Edbali'], 'year': '2018', 'result': True}
+        {
+            'authors': ['Edbali'],
+            'year': '2018',
+            'result': True,
+            'in_text': False,
+            'in_text_separated': False,
+            'parenthetical': True
+        }
     ),
     (
         'The cat is brown (Edbali).',
         'Edbali, T. (2018) "The test" Journal of Testing, 15(4): 261-278.',
-        {'authors': ['Edbali'], 'year': '2018', 'result': False}
+        {
+            'authors': ['Edbali'],
+            'year': '2018',
+            'result': False,
+            'in_text': False,
+            'in_text_separated': False,
+            'parenthetical': False
+        }
     ),
     (
         'The cat is brown (Edbali and Gazzini, 1999).',
-        'Edbali, O. and Gazzini, T. (1999) The test.',
-        {'authors': ['Edbali', 'Gazzini'], 'year': '1999', 'result': True}
+        'Edbali, O. & Gazzini, T. (1999) The test.',
+        {
+            'authors': ['Edbali', 'Gazzini'],
+            'year': '1999',
+            'result': True,
+            'in_text': False,
+            'in_text_separated': False,
+            'parenthetical': True
+        }
     ),
     (
         'Edbali & Gazzini (1999) stated that the cat is brown.',
-        'Edbali, O. & Gazzini, T. (1999) "The test"',
-        {'authors': ['Edbali', 'Gazzini'], 'year': '1999', 'result': True}
+        'Edbali, O. and Gazzini, T. (1999) "The test"',
+        {
+            'authors': ['Edbali', 'Gazzini'],
+            'year': '1999',
+            'result': True,
+            'in_text': True,
+            'in_text_separated': False,
+            'parenthetical': False
+        }
     ),
     (
         'Edbali, Gazzini and Bona (1999) stated that the cat is brown.',
         'Edbali, O., Gazzini, T. and Bona, D. (1999) "The test"',
-        {'authors': ['Edbali', 'Gazzini', 'Bona'], 'year': '1999', 'result': True}
+        {
+            'authors': ['Edbali', 'Gazzini', 'Bona'],
+            'year': '1999',
+            'result': True,
+            'in_text': True,
+            'in_text_separated': False,
+            'parenthetical': False
+        }
     ),
     (
         'The cat is brown (Edbali et al. 1999).',
         'Edbali, O., Gazzini, T., Bona, D. and Buffon, G. (1999) "The test"',
-        {'authors': ['Edbali', 'Gazzini', 'Bona', 'Buffon'], 'year': '1999', 'result': True}
-    )
+        {
+            'authors': ['Edbali', 'Gazzini', 'Bona', 'Buffon'],
+            'year': '1999',
+            'result': True,
+            'in_text': False,
+            'in_text_separated': False,
+            'parenthetical': True
+        }
+    ),
+    (
+        'Edbali, in his 1999 article, stated that salt is bad for the health.',
+        'Edbali, O. (1999) "The test"',
+        {
+            'authors': ['Edbali'],
+            'year': '1999',
+            'result': True,
+            'in_text': False,
+            'in_text_separated': True,
+            'parenthetical': False
+        }
+    ),
+    (
+        'Gazzini ( 2017 ), stated that salt is bad for the health.',
+        'Gazzini, T. (2017) "The test"',
+        {
+            'authors': ['Gazzini'],
+            'year': '2017',
+            'result': True,
+            'in_text': True,
+            'in_text_separated': False,
+            'parenthetical': False
+        }
+    ),
+    (
+        'Salt is bad for your health (Edbali, Gazzini and Bona ,  1999).',
+        'Edbali, O., Gazzini, T. and Bona, D. (1999) "The test"',
+        {
+            'authors': ['Edbali', 'Gazzini', 'Bona'],
+            'year': '1999',
+            'result': True,
+            'in_text': False,
+            'in_text_separated': False,
+            'parenthetical': True
+        }
+    ),
 ]
 
 
@@ -60,6 +145,14 @@ def test_citation_check(text, reference, expected):
     citation_checker = _get_citation_checker(text, reference)
     data = citation_checker.run()
 
+    print('ACTUAL:')
+    print(data)
+    print('EXPECTED:')
+    print(expected)
+
     assert data.get('result') == expected.get('result')
     assert set(data.get('authors')) == set(expected.get('authors'))
     assert data.get('year') == expected.get('year')
+    assert data.get('in_text') == expected.get('in_text')
+    assert data.get('in_text_separated') == expected.get('in_text_separated')
+    assert data.get('parenthetical') == expected.get('parenthetical')
