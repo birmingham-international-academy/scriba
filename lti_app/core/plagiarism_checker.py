9eb883d0744e22df31720838538c0d835595d8e2
oss6 <ossedb@gmail.com>
2018-08-12 19:05:04 +0100

    Add plagiarism tests

--- lti_app/core/plagiarism_checker.py
+++ lti_app/core/plagiarism_checker.py
@@ -4,9 +4,11 @@ The minimum requirement of a plagiarism checker is to highlight
 repeated word strings from two texts.
 """
 
+import difflib
 from difflib import ndiff
 
 from lti_app import strings
+from lti_app.core.text_processing.tools import Tools
 
 
 class Checker:
@@ -20,39 +22,34 @@ class Checker:
     def __init__(self, text_document, excerpt_document):
         self.text_document = text_document
         self.excerpt_document = excerpt_document
+        self.tools = Tools()
 
-    def get_matches(self):
-        """Get the diff matches.
+    def matches(self, list1, list2, min_length=4):
+        words1, lemmas1 = [list(ls) for ls in list(zip(*list1))]
+        words2, lemmas2 = [list(ls) for ls in list(zip(*list2))]
 
-        Returns:
-            list of str: Similar strings using the diff tool.
-        """
+        lemmas1 = [lemma.lower() for lemma in lemmas1]
+        lemmas2 = [lemma.lower() for lemma in lemmas2]
 
-        matches = []
-        match = []
-        index = -1
-        min_length = 4
+        while True:
+            mbs = difflib.SequenceMatcher(None, lemmas1, lemmas2).get_matching_blocks()
 
-        text_lemmas = [lemma for _, lemma in self.text_document.get(strings.lemmas)]
-        excerpt_lemmas = [lemma for _, lemma in self.excerpt_document.get(strings.lemmas)]
+            if len(mbs) == 1:
+                break
 
-        for i, d in enumerate(ndiff(text_lemmas, excerpt_lemmas)):
-            if d[0] == ' ':
-                word = d.split(' ')[-1]
+            for i, j, n in mbs[::-1]:
+                if n >= min_length:
+                    yield words1[i: i + n]
 
-                if i == index + 1:
-                    match.append(word)
-                else:
-                    if len(match) >= min_length:
-                        matches.append(' '.join(match))
-                    match = [word]
-
-                index = i
-
-        if len(match) >= min_length:
-            matches.append(' '.join(match))
-
-        return matches
+                del words1[i: i + n]
+                del lemmas1[i: i + n]
+                del words2[j: j + n]
+                del lemmas2[j: j + n]
 
     def run(self):
-        return self.get_matches()
+        text_lemmas = self.text_document.get(strings.lemmas)
+        excerpt_lemmas = self.excerpt_document.get(strings.lemmas)
+        matches = list(self.matches(text_lemmas, excerpt_lemmas))
+        detokenize = self.tools.word_detokenizer.detokenize
+
+        return [detokenize(match) for match in matches]
