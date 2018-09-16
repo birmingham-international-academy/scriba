bd07143aa065055204aa69510079419b6f9c6ad8
oss6 <ossedb@gmail.com>
2018-09-16 16:22:45 +0100

    Remove import

--- scriba/settings/base.py
+++ scriba/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import json
-from django.core.exceptions import ImproperlyConfigured
+# from django.core.exceptions import ImproperlyConfigured
 from dotenv import load_dotenv
 
 
